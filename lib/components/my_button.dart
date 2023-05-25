89b8fdb50ab0261c54c3a9e3d7b081fe5099284f
1ayuu <aiyanakoirala8@gmail.com>
2023-05-25 09:54:03 +0545

    edit

---
+++ lib/components/my_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 90,
+        height: 42.0,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: const Color(0xff58B773),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
