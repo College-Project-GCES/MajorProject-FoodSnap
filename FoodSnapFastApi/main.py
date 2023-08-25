c6f8e35af66fb3ab2ad13c735d1b6b947e0fa080
Pratigya <pratigyadhakal4444@gmail.com>
2023-08-25 17:13:46 +0545

    added nutrition and diabetic recommendation

--- FoodSnapFastApi/main.py
+++ FoodSnapFastApi/main.py
@@ -6,7 +6,8 @@ from utils import load_and_prep, get_classes
 import pandas as pd
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 import base64
-
+import json
+import os
 
 app = FastAPI()
 
@@ -16,6 +17,29 @@ app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 # Load and prepare model
 model = tf.keras.models.load_model("./models/EfficientNetB1.hdf5")
 class_names = get_classes()
+
+# Load nutrition data and diabetic recommendations
+with open('diabetic.json', 'r') as file:
+    diabetic_recommendations = json.load(file)
+
+nutrition_data = pd.read_csv('nutrition.csv')
+
+def retrieve_nutrition_values(food_category):
+    # Preprocess the food_category: remove special characters, spaces, and convert to lowercase
+    food_category_processed = food_category.lower().replace("-", "").replace("_", "").replace(" ", "")
+    
+    nutrition_values = nutrition_data[nutrition_data['product_name'].str.lower().str.replace("-", "").str.replace("_", "").str.replace(" ", "") == food_category_processed]
+    if nutrition_values.empty:
+        return None  # Return None if no nutrition values are available
+    return nutrition_values
+
+def get_diabetic_recommendation(food_category):
+    food_category_lower = food_category.lower()
+    
+    for category in diabetic_recommendations:
+        if category['name'].lower() == food_category_lower or food_category_lower in [name.lower() for name in category['alternateNames']]:
+            return category
+    return None  # Return None if no diabetic recommendation is available
 
 def predicting(image, model):
     image = load_and_prep(image)
@@ -103,16 +127,60 @@ async def predict(file: UploadFile = File(...)):
      
     # Convert the image data to Base64
     image_base64 = base64.b64encode(image).decode('utf-8')
-
+    
     chart = alt.Chart(df).mark_bar().encode(
         x='F1 Scores',
         y=alt.X('Top 5 Predictions', sort=None),
         color=alt.Color("color", scale=None),
         text='F1 Scores'
     ).properties(width=600, height=400)
-    
     chart_html = chart.to_html()
+    nutrition_values = retrieve_nutrition_values(pred_class)
+    nutrition_table = ""
+    if nutrition_values is not None and not nutrition_values.empty:
+        nutrition_table += "<h3>Nutrition Values:</h3>"
+        nutrition_table += "<table>"
+        nutrition_table += "<tr><th>Product Name</th><th>Energy</th><th>Carbohydrates</th><th>Sugars</th><th>Proteins</th><th>Fat</th><th>Fiber</th><th>Cholesterol</th></tr>"
+        for index, row in nutrition_values.iterrows():
+            nutrition_table += "<tr>"
+            nutrition_table += f"<td>{row['product_name']}</td>"
+            nutrition_table += f"<td>{row['energy_100g']} kJ</td>"
+            nutrition_table += f"<td>{row['carbohydrates_100g']} g</td>"
+            nutrition_table += f"<td>{row['sugars_100g']} g</td>"
+            nutrition_table += f"<td>{row['proteins_100g']} g</td>"
+            nutrition_table += f"<td>{row['fat_100g']} g</td>"
+            nutrition_table += f"<td>{row['fiber_100g']} g</td>"
+            nutrition_table += f"<td>{row['cholesterol_100g']} mg</td>"
+            nutrition_table += "</tr>"
+        nutrition_table += "</table>"
+    
+    diabetic_recommendations = ""
+    if nutrition_values is not None:
+        recommendation = get_diabetic_recommendation(pred_class)
+        if recommendation is not None:
+            diabetic_recommendations += "<h3>Diabetic Recommendations:</h3>"
+            level_info = {
+                1: ("Avoid", "Diabetic people should stay away from this food."),
+                2: ("Caution", "This food can be eaten in moderation or under certain conditions."),
+                3: ("Okay", "Neutral. No significant positive or negative health effects."),
+                4: ("Good", "Food that is good for a diabetic person to eat."),
+                5: ("Great", "Foods at this level are among the healthiest options.")
+            }
 
+            level = recommendation['level']
+            level_info_tuple = level_info.get(level, ("Unknown", "No information available for this level."))
+            level_name = level_info_tuple[0]
+            level_reason = level_info_tuple[1]
+
+            diabetic_recommendations += f"<p><strong>Recommendation Level:</strong> {level_name}</p>"
+            diabetic_recommendations += f'<button id="more-info-button-{pred_class}" onclick="toggleInfo(\'{pred_class}\')">More Info</button>'
+            diabetic_recommendations += f'<div id="info-{pred_class}" style="display: none;">'
+            diabetic_recommendations += f"<p><strong>Reason:</strong><br>{level_reason}</p>"
+            diabetic_recommendations += f"<p><strong>Explanation:</strong><br>{recommendation['explanation']}</p>"
+            diabetic_recommendations += "<h4>Suggestions:</h4>"
+            diabetic_recommendations += recommendation['suggestions'].replace("\n", "<br>")
+            diabetic_recommendations += '</div>'  
+            
     return HTMLResponse(content=f"""
     <html>
     <head>
@@ -138,16 +206,27 @@ async def predict(file: UploadFile = File(...)):
         border-radius: 5px;
     }}
     </style>
+    <script>
+    function toggleInfo(food) {{
+        var infoDiv = document.getElementById('info-' + food);
+        var button = document.getElementById('more-info-button-' + food);
+        if (infoDiv.style.display === 'none') {{
+            infoDiv.style.display = 'block';
+            button.innerText = 'Less Info';
+        }} else {{
+            infoDiv.style.display = 'none';
+            button.innerText = 'More Info';
+        }}
+    }}
+    </script>
     </head>
     <body>
     <h2>Prediction: {pred_class}</h2>
     <p>Confidence: {pred_conf*100:.2f}%</p>
     <img src="data:image/jpeg;base64,{image_base64}" alt="Uploaded Image" width="400">
     <div>{chart_html}</div>
+    {nutrition_table}
+    {diabetic_recommendations}
     </body>
     </html>
     """)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
